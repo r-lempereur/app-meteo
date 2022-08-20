57e8d79ec0d0a0b535e38d121d8e86589356f3d2
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2022-08-20 14:09:20 +0200

    Correction affichage erreur champ vide

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -31,50 +31,52 @@ class DefaultController extends AbstractController
         $form->handleRequest($request);
         $infos = array("error" => false, "message" => "");
 
-        if ($form->isSubmitted() && $form->getData()['search'] !== "") {
-            setlocale(LC_TIME, 'fr_FR');
-            date_default_timezone_set('Europe/Paris');
-            $city = $form->getData()['search'];
-            $dateNow = getdate();
-            $searchResult = $searchService->searchByLocalisation($city, $dateNow);
-            $filename = "meteo-".strtolower($city);
+        if ($form->isSubmitted()) {
+            if($form->getData()['search'] && $form->getData()['search'] !== ""){
+                setlocale(LC_TIME, 'fr_FR');
+                date_default_timezone_set('Europe/Paris');
+                $city = $form->getData()['search'];
+                $dateNow = getdate();
+                $searchResult = $searchService->searchByLocalisation($city, $dateNow);
+                $filename = "meteo-".strtolower($city);
 
-            if($searchResult){
-                if(isset($searchResult['new'])){
-                    // Création du template pour l'image
-                    $content = $this->renderView('Image/index.html.twig', array(
-                        "date" => $dateManager->getDateFormatFr($dateNow),
-                        "data_icon" => base64_encode($generateImage->getIcone($asset . $searchResult["icon"] . ".png")),
-                        "temp" => $searchResult["temp"],
-                        "localisation" =>  $city
+                if($searchResult){
+                    if(isset($searchResult['new'])){
+                        // Création du template pour l'image
+                        $content = $this->renderView('Image/index.html.twig', array(
+                            "date" => $dateManager->getDateFormatFr($dateNow),
+                            "data_icon" => base64_encode($generateImage->getIcone($asset . $searchResult["icon"] . ".png")),
+                            "temp" => $searchResult["temp"],
+                            "localisation" =>  $city
+                        ));
+
+                        // Génération de l'image
+                        $pathPng = $generateImage->generateSnappyImage($filename, $content);
+
+                        // Sauvegarde des données
+                        $dataSaveManager->addCityData($city, $filename, $pathPng);
+                        $dataSaveManager->saveCityData();
+                    } else {
+                        // Retourne l'image directement
+                        $pathPng = $dataSaveManager->getCityData()[$city]["pathPng"];
+                    }
+
+                    $stream = new Stream($pathPng);
+                    return new BinaryFileResponse($stream, 200, array(
+                        "Content-Type" => "image/png",
+                        "Content-Disposition" => "Attachment;filename=$filename.png"
                     ));
 
-                    // Génération de l'image
-                    $pathPng = $generateImage->generateSnappyImage($filename, $content);
-
-                    // Sauvegarde des données
-                    $dataSaveManager->addCityData($city, $filename, $pathPng);
-                    $dataSaveManager->saveCityData();
                 } else {
-                    // Retourne l'image directement
-                    $pathPng = $dataSaveManager->getCityData()[$city]["pathPng"];
+                    // Erreur lors de la récupération des données API WEATHER
+                    $infos['error'] = true;
+                    $infos['message'] = "Il n'y a pas de données pour la ville saisie, veuillez essayer une autre ville.";
                 }
-
-                $stream = new Stream($pathPng);
-                return new BinaryFileResponse($stream, 200, array(
-                    "Content-Type" => "image/png",
-                    "Content-Disposition" => "Attachment;filename=$filename.png"
-                ));
-
             } else {
-                // Erreur lors de la récupération des données API WEATHER
+                // Erreur la saisie est vide
                 $infos['error'] = true;
-                $infos['message'] = "Il n'y a pas de données pour la ville saisie, veuillez essayer une autre ville.";
+                $infos['message'] = "Le champ Localisation est obligatoire";
             }
-        } else {
-            // Erreur la saisie est vide
-            $infos['error'] = true;
-            $infos['message'] = "Le champ Localisation est obligatoire";
         }
 
         return $this->renderForm('Default/index.html.twig', array(
